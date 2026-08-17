c21f3c65a107fc652bfbc4381f643fc9d0b47392
Gabriel do Espirito Santo <gabriel.e.santo007@gmail.com>
2026-08-17 19:53:18 -0300

    fix: arrumando nome errado da biblioteca

--- gerenciadorEventos/src/main.jsx
+++ gerenciadorEventos/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {GoogleAuthProvider} from '@react-oauth/google';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
-    </GoogleAuthProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
+  
